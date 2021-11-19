70d32d23d062295b5839836fab8ba322d3f1f3ce
sajjad-ehsanian <sa.ehsanian@gmail.com>
2021-11-19 12:53:00 +0330

    Initial Commit

---
+++ sajad/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  sajad
+//
+//  Created by sajad ehsanian on 8/28/1400 AP.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ sajad/sajadApp.swift
@@ -0,0 +1,17 @@
+//
+//  sajadApp.swift
+//  sajad
+//
+//  Created by sajad ehsanian on 8/28/1400 AP.
+//
+
+import SwiftUI
+
+@main
+struct sajadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
